3e86ded8241dc13ae89b46e0a95a378196d0c4a1
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2022-04-04 14:54:27 +0300

    qq

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -51,6 +51,32 @@ else
 
 
 
+Func<int, int, string> SetCoordinates = (x, y) => { return $"X: {x}, Y: {y}"; };
+
+Console.WriteLine(SetCoordinates(2, 3));
+
+SetCoordinates = (x, y) => Execute(x, y);
+
+Console.WriteLine(SetCoordinates(5, 5));
+
+
+SetCoordinates = (x, y) => Divide(x, y);
+
+Console.WriteLine(SetCoordinates(5, 5));
+
+
+static string Execute(int x, int y)
+{
+  return "Only x: "+x.ToString();
+}
+
+static string Divide(int x, int y)
+{
+  return y > 0 ? "Division: "+(x / y).ToString() : 0.ToString(); 
+}
+
+
+
 
 
 
